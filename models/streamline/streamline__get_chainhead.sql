4a35a723bc98933bd0fa9e0b7284ed4232a8d22f
Austin <austin@flipsidecrypto.com>
2024-10-01 16:51:59 -0400

    complete and chainhead

--- models/streamline/streamline__get_chainhead.sql
+++ models/streamline/streamline__get_chainhead.sql
@@ -1,8 +1,1 @@
-{{ config (
-    materialized = 'table',
-    tags = ['streamline_core_complete']
-) }}
-{{ fsc_evm.streamline_core_chainhead(
-    vault_secret_path = var('VAULT_SECRET_PATH'),
-    api_url = var('API_URL')
-) }}
+{{ fsc_evm.streamline_core_chainhead_v2() }}
