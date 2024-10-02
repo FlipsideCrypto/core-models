24d0a650c7b87aab1398a6566b5e3e8fa05d53f3
Austin <austin@flipsidecrypto.com>
2024-10-01 23:56:25 -0400

    2

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -1,7 +1,7 @@
 {% macro create_sps() %}
     {% if var("UPDATE_UDFS_AND_SPS", false) %}
-        {% set current_profile = var('PROD_DB_NAME') | upper %}
-        {% if target.database | upper == current_profile and target.name == 'prod' %}
+        {% set prod_db_name = var('PROD_DB_NAME') | upper %}
+        {% if target.database | upper == prod_db_name and target.name == 'prod' %}
             {% set schema_name = var("SPS_SCHEMA_NAME", '_internal') %}
             CREATE SCHEMA IF NOT EXISTS {{ schema_name }};
             {{ sp_create_prod_clone(schema_name) }};
