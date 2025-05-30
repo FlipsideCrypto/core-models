4ed194b13e92ccb2651dfc74b53eefa96500f59e
San Yong <22216004+SanYongxie@users.noreply.github.com>
2025-05-30 10:48:42 +0800

    initial commit

---
+++ models/silver/defi/dex/corex/silver_dex__corex_pools.sql
@@ -0,0 +1,62 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'pool_address',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated']
+) }}
+
+WITH pool_creation AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT('0x', SUBSTR(topic_1, 27, 40)) AS token0,
+        CONCAT('0x', SUBSTR(topic_2, 27, 40)) AS token1,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(topic_3)) AS fee,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(segmented_data [0] :: STRING)) AS tick_spacing,
+        CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 25, 40)) AS pool_address,
+        CONCAT(
+            tx_hash :: STRING,
+            '-',
+            event_index :: STRING
+        ) AS _log_id,
+        modified_timestamp AS _inserted_timestamp
+    FROM
+        {{ ref('core__fact_event_logs') }}
+    WHERE
+        contract_address = '0x526190295afb6b8736b14e4b42744fbd95203a3a'
+        AND topic_0 = '0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118'
+        AND tx_succeeded
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+AND _inserted_timestamp >= SYSDATE() - INTERVAL '7 day'
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    contract_address,
+    event_index,
+    token0,
+    token1,
+    pool_address,
+    fee,
+    tick_spacing,
+    _log_id,
+    _inserted_timestamp
+FROM
+    pool_creation qualify(ROW_NUMBER() over (PARTITION BY pool_address
+ORDER BY
+    _inserted_timestamp DESC)) = 1
